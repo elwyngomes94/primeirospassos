ee4d788db5ca5d71f050d5156cbc3ed6f90f5b32
Elwyn <91758664+elwyngomes94@users.noreply.github.com>
2021-10-02 22:58:26 -0300

    Add files via upload

--- projeto.py
+++ projeto.py
@@ -1,4 +1,12 @@
-num1 = int (input("digite o primeiro número:"))
-num2 = int (input("digite o segundo número:"))
-soma = (num1+num2)
-print("O valor da Soma é:",soma)
+
+sexo = int(input("Escolha: 1- Sexo Masculino / 2- Sexo Feminino: "))
+h = float(input("Altura:"))
+
+
+peso_ideal = (72.7*h) - 58 if sexo == 1 else (62.1*h) - 44.7
+
+if sexo == 1:
+	print("seu peso ideal é:",peso_ideal)
+else:
+	print("seu peso ideal é:",peso_ideal)
+
---
+++ projetos.py
@@ -0,0 +1,4 @@
+raio = float(input ("Digite o Raio do Circulo:"))**2
+pi = float (3.14)
+area = float(pi * raio)
+print("A área do circulo é:",area)
